c93e1a8e81e7214ed612933c11eb62a9e5b940a9
AdarDesu <adartruusa@gmail.com>
2022-09-17 03:02:52 +0300

    First app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code: ＞︿＜
+// Application asks for your name.
+// Application greets the user by their name. 
+
+Console.WriteLine("Enter your name:"); // Output
+
+// String - stores word type of data.
+
+string username = Console.ReadLine(); // Input
+Console.WriteLine("Okay, so you are " + username + "!"); // Output
+
+// String interpolation (Better thing to use.)
+
+Console.WriteLine($"Okay, so you are {username}!"); // Output
